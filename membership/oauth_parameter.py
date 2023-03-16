38ce3e34b290f2f5abd38b14209fb4fdc61453ca
Park donghyeon <emma415g@gmail.com>
2023-03-16 23:32:57 +0900

    Fixed kakao oauth redirect url

--- membership/oauth_parameter.py
+++ membership/oauth_parameter.py
@@ -1,4 +1,5 @@
 KAKAO_OAUTH_URI = "https://kauth.kakao.com/oauth/authorize?response_type=code"
-KAKAO_OAUTH_REDIRECT_URI = "http://127.0.0.1:8000/users/login/kakao/callback"
+# KAKAO_OAUTH_REDIRECT_URI = "http://127.0.0.1:8000/users/login/kakao/callback"
+KAKAO_OAUTH_REDIRECT_URI = "https://mindpairing-django.herokuapp.com/users/login/kakao/callback"
 KAKAO_OAUTH_CLIENT_ID = "d7fc0fc15801fe630bf38c9a381931f2"
 KAKAO_OAUTH_TOKEN_API = "https://kauth.kakao.com/oauth/token"
